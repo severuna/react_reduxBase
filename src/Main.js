4bf9073de17f9c21e691cd90e7e8e88f079f7953
Severuna <oxxxyfomenkova@gmail.com>
2023-05-13 22:44:23 +0300

    add Main & move init tile & styles

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className='main row'>
+            <h1 className='title'>Init title</h1>
+        </div>
+    );
+};
+
+export default Main;
