62be184d0962faf6857b353e79c9bd5a0ac907c8
Severuna <oxxxyfomenkova@gmail.com>
2023-05-14 20:42:55 +0300

    add components folder & move to Home & Add

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Main.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
-import Add from './AddPost/Add';
+import Home from './pages/Home/Home';
+import Add from './pages/AddPost/Add';
 
 const Main = () => {
     return (
