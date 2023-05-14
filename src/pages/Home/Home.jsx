0325edb07fc977c2d9084270e90743b64e707057
Severuna <oxxxyfomenkova@gmail.com>
2023-05-14 23:00:57 +0300

    get reducer in Home & output console (temp)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+    const AUTHOR_LIST = useSelector((state) => state);
+    console.log({AUTHOR_LIST});
+    
     return (
         <div className='content column'>
             <h1 className='title'>Home (show posts)</h1>
