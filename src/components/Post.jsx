1cea13218b728fbd185171ea7b5959fd244a4e84
Severuna <oxxxyfomenkova@gmail.com>
2023-05-15 01:07:44 +0300

    add init Post styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,20 +6,29 @@ const Post = ( props ) => {
     return (
         <div className='post column'>
             <div className='post-head row'>
-                <img src='' alt={props.name} className='post-head__avatar' />
+                <img src={props.avatar} alt={props.name} className='post-head__avatar' />
                 <h2 className="post-head__name">{props.name}</h2>
-                <p className='post-head__nickname'>{props.nickname}</p>
+                <p className='post-head__nickname'>@{props.nickname}</p>
                 <p className='post-head__date'>{props.date}</p>
             </div>
             <div className='post-main column'>
                 <div className='post-main__content column'>
                     <p className='post-main__content-text'>{props.content}</p>
-                    <img src='' alt='' className='post-main__content-media' />
+                    <img src={props.media} alt='media' className='post-main__content-media' />
                 </div>
                 <div className='post-main__reactions row'>
-                    <Reactions alt='like' />
-                    <Reactions variant='comment' alt='comment' />
+                    <div className='post-main__reactions-item row'>
+                        <Reactions alt='like' />
+                        <p>{props.like}</p>
+                    </div>
+                    <div className='post-main__reactions-item row'>
+                        <Reactions variant='comment' alt='comment' />
+                        <p>{props.comments}</p>
+                    </div>
+                    <div className='post-main__reactions-item row'>
                     <Reactions variant='retweet' alt='retweet' />
+                        <p>{props.retweet}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './Home.css';
+import Post from '../../components/Post';
 
 const Home = () => {
 
     const AUTHOR_LIST = useSelector((state) => state);
     console.log({AUTHOR_LIST});
+
+    const obj = {
+        name: 'Draco Malfoy',
+        nickname: 'best',
+        date: '09.10.2022',
+        content: 'You`re not dangerous at all, are you, you great ugly brute!',
+        avatar: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/draco-malfoy-from-harry-potter-watercolor-portrait-design-turnpike.jpg',
+        media: 'https://f.vividscreen.info/soft/7fe06adc5f4d516932fb6d19adbd5166/Cute-Kitty-Art-wide-i.jpg',
+        like: 451,
+        comments: 561,
+        retweet: 501
+    }
     
     return (
         <div className='content column'>
             <h1 className='title'>Home (show posts)</h1>
+            <Post {...obj}/>
         </div>
     );
 };
